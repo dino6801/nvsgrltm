f757432083b34a4e21697dc9cb6b33822e029cfb
dino6801 <148502677+dino6801@users.noreply.github.com>
2023-10-20 08:56:31 -0500

    Cambio Redirección php

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
+
 // Define la URL de destino
 $url = 'http://novasegurolatam.com/';
 
-// Establece la cookie
-setcookie('redirect_url', $url, time() + 300);
-
-// Muestra un mensaje de cargando animado
-echo '<div class="cargando">Cargando...</div>';
+// Redirige al usuario
+header('Location: ' . $url);
 
 // Finaliza el script
 exit();
